ccfa144715247ca457c03dbee76c709bcd395fb6
ToroHack <torohack.github@gmail.com>
2025-04-27 20:30:53 +0200

    feature: display public URL to S3 resource

--- helpers.py
+++ helpers.py
@@ -7,9 +7,11 @@ def set_img(url, caption_img, subheader_img=None):
     response = requests.get(url)
     img_bytes = response.content
     st.image(img_bytes, caption=caption_img)
+    st.markdown(f'[URL pública al recurso en S3]({url})')
 
 def set_video(subheader_video, url):
     st.subheader(subheader_video)
     response = requests.get(url)
     video_bytes = response.content
     st.video(video_bytes)
+    st.markdown(f'[URL pública al recurso en S3]({url})')
